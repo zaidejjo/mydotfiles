a464dace1c674e6527d19398fbbc2db2ea92bed6
zaidejjo <zaidejjo@gmail.com>
2026-07-19 00:51:14 +0300

    Updated

--- nvim/.config/nvim/lua/plugins/example.lua
+++ nvim/.config/nvim/lua/plugins/example.lua
@@ -31,14 +31,14 @@ return {
   { "folke/trouble.nvim", enabled = false },
 
   -- override nvim-cmp and add cmp-emoji
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = { "hrsh7th/cmp-emoji" },
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     table.insert(opts.sources, { name = "emoji" })
+  --   end,
+  -- },
 
   -- change some telescope options and a keymap to browse plugin files
   {
